0560b4dd0ff04bfda03e447f3e7b236bac9373d6
wenjian.ma <wenjian.ma@denglin.ai>
2025-07-09 15:14:34 +0800

    for test test_llama_cpp.sh

--- .dlci/test_llama_cpp.sh
+++ .dlci/test_llama_cpp.sh
@@ -10,6 +10,13 @@ ccache --zero-stats
 # --- test-backend-ops ---
 build_dir=$(pwd)/../build
 
+if [ ! -d "${build_dir}" ]; then
+    echo "[ERROR] $build_dir is not exist ..."
+    exit 1
+fi
+
+echo "Pass 1111111111"
+
 case_file=${build_dir}/bin/test-backend-ops
 
 # Ensure case_file is valid
@@ -17,15 +24,20 @@ if [ ! -f "$case_file" ]; then
   echo "[ERROR] $case_file is not exist ..."
   exit 1
 fi
+echo "Pass 222222222"
 
 export PATH=${build_dir}/bin:$PATH
 export LD_LIBRARY_PATH=${build_dir}/bin:$LD_LIBRARY_PATH
 
 chmod +x case_file
 
+echo "Pass 333333333"
+
 ldd $case_file | grep -q "not found"
 
 cd ${build_dir}/bin
+
+echo "Pass 444444444444"
 
 ./test-backend-ops
 
