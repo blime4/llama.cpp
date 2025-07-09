23f2a46fc991ac84e10daa29d6b8518364e1a5ca
Xuan-Son Nguyen <son@huggingface.co>
2025-07-09 18:16:12 +0200

    ggml : add ggml_scale_bias (#14417)
    * ggml : add ggml_scale_bias
    
    * ggml_vec_mad1_f32
    
    * add more simd
    
    * add CUDA
    
    * sycl
    
    * vulkan
    
    * cann (placeholder)
    
    * opencl
    
    * will this fix cpu?
    
    * fix cuda
    
    * suggestions from coderabbit
    
    * fix cann compile error
    
    * vDSP_vsmsa
    
    * rm __ARM_FEATURE_SVE
    
    * use memcpy for op params
    
    * make code looks more consistent
    
    * use scalar for __ARM_FEATURE_SVE
    
    * add x param to ggml_vec_mad1_f32

--- ggml/src/ggml-cuda/scale.cu
+++ ggml/src/ggml-cuda/scale.cu
@@ -1,20 +1,18 @@
 #include "scale.cuh"
 
-template <typename T>
-static __global__ void scale_kernel(const T * x, T * dst, const float scale, const int k) {
+static __global__ void scale_f32(const float * x, float * dst, const float scale, const float bias, const int k) {
     const int i = blockDim.x*blockIdx.x + threadIdx.x;
 
     if (i >= k) {
         return;
     }
 
-    dst[i] = static_cast<T>(scale * static_cast<float>(x[i]));
+    dst[i] = scale * x[i] + bias;
 }
 
-template <typename T>
-static void scale_cuda(const T * x, T * dst, const float scale, const int k, cudaStream_t stream) {
+static void scale_f32_cuda(const float * x, float * dst, const float scale, const float bias, const int k, cudaStream_t stream) {
     const int num_blocks = (k + CUDA_SCALE_BLOCK_SIZE - 1) / CUDA_SCALE_BLOCK_SIZE;
-    scale_kernel<<<num_blocks, CUDA_SCALE_BLOCK_SIZE, 0, stream>>>(x, dst, scale, k);
+    scale_f32<<<num_blocks, CUDA_SCALE_BLOCK_SIZE, 0, stream>>>(x, dst, scale, bias, k);
 }
 
 void ggml_cuda_op_scale(ggml_backend_cuda_context & ctx, ggml_tensor * dst) {
@@ -28,13 +26,9 @@ void ggml_cuda_op_scale(ggml_backend_cuda_context & ctx, ggml_tensor * dst) {
     GGML_ASSERT(src0->type == dst->type);
 
     float scale;
-    memcpy(&scale, dst->op_params, sizeof(float));
+    float bias;
+    memcpy(&scale, (float *) dst->op_params + 0, sizeof(float));
+    memcpy(&bias,  (float *) dst->op_params + 1, sizeof(float));
 
-    if (src0->type == GGML_TYPE_F16) {
-        scale_cuda((const half*)src0_d, (half*)dst_d, scale, ggml_nelements(src0), stream);
-    } else if (src0->type == GGML_TYPE_F32) {
-        scale_cuda(src0_d, dst_d, scale, ggml_nelements(src0), stream);
-    } else {
-        GGML_ASSERT(false);
-    }
+    scale_f32_cuda(src0_d, dst_d, scale, bias, ggml_nelements(src0), stream);
 }
