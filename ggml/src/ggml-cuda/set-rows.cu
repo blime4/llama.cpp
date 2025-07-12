800a94876273dc5bbeee022f0f2a4de74ae24852
Aman Gupta <amangupta052@gmail.com>
2025-07-12 21:31:38 +0800

    CUDA: add set rows for f32 and f16 (#14551)
    * CUDA: add set rows for f32 and f16
    
    * Review: change kernel params, use strides from host
    
    * Use 1-d kernel
    
    * Review: use int64_t for blockDim.x, rename nb->s for clarity

--- ggml/src/ggml-cuda/set-rows.cu
+++ ggml/src/ggml-cuda/set-rows.cu
@@ -124,16 +124,6 @@ void ggml_cuda_op_set_rows(ggml_backend_cuda_context & ctx, ggml_tensor * dst) {
             nb1, nb2, nb3,
             stream
         );
-    } else if (dst->type == GGML_TYPE_BF16) {
-        set_rows_cuda(
-            src0_d, src1_d, (nv_bfloat16*)dst->data,
-            ne00, ne01, ne02, ne03,
-            ne10, ne11, ne12, ne13,
-            nb01, nb02, nb03,
-            nb10, nb11, nb12,
-            nb1, nb2, nb3,
-            stream
-        );
     } else {
         GGML_ABORT("unsupported type");
     }
