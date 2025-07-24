aa99a323419e1581214e18bb8a32edb61d4e441e
R0CKSTAR <yeahdongcn@gmail.com>
2025-07-25 03:05:37 +0800

    musa: upgrade musa sdk to rc4.2.0 (#14498)
    * musa: apply mublas API changes
    
    Signed-off-by: Xiaodong Ye <xiaodong.ye@mthreads.com>
    
    * musa: update musa version to 4.2.0
    
    Signed-off-by: Xiaodong Ye <xiaodong.ye@mthreads.com>
    
    * musa: restore MUSA graph settings in CMakeLists.txt
    
    Signed-off-by: Xiaodong Ye <xiaodong.ye@mthreads.com>
    
    * musa: disable mudnnMemcpyAsync by default
    
    Signed-off-by: Xiaodong Ye <xiaodong.ye@mthreads.com>
    
    * musa: switch back to non-mudnn images
    
    Signed-off-by: Xiaodong Ye <xiaodong.ye@mthreads.com>
    
    * minor changes
    
    Signed-off-by: Xiaodong Ye <xiaodong.ye@mthreads.com>
    
    * musa: restore rc in docker image tag
    
    Signed-off-by: Xiaodong Ye <xiaodong.ye@mthreads.com>
    
    ---------
    
    Signed-off-by: Xiaodong Ye <xiaodong.ye@mthreads.com>

--- ggml/src/ggml-cuda/common.cuh
+++ ggml/src/ggml-cuda/common.cuh
@@ -781,9 +781,7 @@ struct ggml_tensor_extra_gpu {
 };
 
 
-// #if ((CUDART_VERSION >= 12000) && defined(GGML_CUDA_USE_GRAPHS)) || defined(GGML_HIP_GRAPHS)
-#if defined(GGML_CUDA_USE_GRAPHS) || defined(GGML_HIP_GRAPHS)
-// #if (!defined(GGML_USE_DLCU))
+#if (defined(GGML_CUDA_USE_GRAPHS) || defined(GGML_HIP_GRAPHS)) || defined(GGML_MUSA_GRAPHS)
 #define USE_CUDA_GRAPH
 #endif
 
