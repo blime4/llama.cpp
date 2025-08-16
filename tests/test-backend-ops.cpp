0c8e28030ff84ee3b866aabcfa3b7df67cd4d795
Jeff Bolz <jbolz@nvidia.com>
2025-08-16 03:58:38 -0500

    vulkan: Add missing bounds checking to scalar/coopmat1 mul_mat_id (#15334)

--- tests/test-backend-ops.cpp
+++ tests/test-backend-ops.cpp
@@ -6440,6 +6440,8 @@ static std::vector<std::unique_ptr<test_case>> make_test_cases_eval() {
     }
     }
 
+    test_cases.emplace_back(new test_mul_mat_id(GGML_TYPE_F16, GGML_TYPE_F32, 1, 1, false, 8, 16, 1));
+
     for (ggml_type type_a : base_types) {
         for (ggml_type type_b : {GGML_TYPE_F32 /*, GGML_TYPE_F16 */}) {
             for (int n_mats : {4, 8}) {
