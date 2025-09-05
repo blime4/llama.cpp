41f104a03c6c07c1fef146e90629c831fe358698
Georgi Gerganov <ggerganov@gmail.com>
2025-09-05 10:39:22 +0300

    kv-cache : fix SWA checks + disable cacheless iSWA (#15811)
    ggml-ci

--- src/llama-kv-cache.h
+++ src/llama-kv-cache.h
@@ -94,6 +94,7 @@ public:
                      uint32_t   n_seq_max,
                      uint32_t   n_pad,
                      uint32_t   n_swa,
+               llama_swa_type   swa_type,
         const layer_filter_cb & filter,
         const  layer_reuse_cb & reuse);
 
